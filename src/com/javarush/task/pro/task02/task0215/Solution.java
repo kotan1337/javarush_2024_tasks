34f9b0dde442ba0005386ab255ad8f2968280163
kotan1337 <stvn.frst@yandex.ru>
2024-02-02 15:59:01 +0300

    started level 2 tasks

---
+++ src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int n3 = scanner.nextInt();
+        int average = (n1 + n2 + n3) / 3;
+
+        System.out.println(average);
+    }
+}
